35b4171ae8322101371992d10f35cebb2f3f7e44
Emmanuel Lim <emmanuel.lim.bridgeoma@gmail.com>
2019-03-15 14:22:19 -0400

    api_name test

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "api_name": "Metabraid Loom v-0.0.1"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "api_name": "Metabraid Loom v-0.0.1"
   });
 });
 
