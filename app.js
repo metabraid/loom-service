afc459871a57fce737e5f4a7853c5ae850258cf4
Emmanuel Lim <emmanuel.lim.bridgeoma@gmail.com>
2019-03-19 09:51:23 -0400

    test resources array

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const resources = [
+  { name: 'code_repo', description: 'URI for the repo' },
+  { name: 'source_codes', description: 'array of source code paths' }
+]
+
 app.get('/', function(req, res) {
   res.send({
     "api_name": "Metabraid Loom v-0.0.1"
   });
+  res.send(resources);
 });
 
 app.post('/', function(req, res) {
