08647f41eb90e8746410acb12e7ce8aaf8d5a4e8
Emmanuel Lim <emmanuel.lim.bridgeoma@gmail.com>
2019-03-19 13:49:36 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -9,6 +9,7 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
+        test.string(result.body.api_name).contains('Metabraid');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
